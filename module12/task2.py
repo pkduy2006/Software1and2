11e88a37103aadf3813eb0a2a8f5ada42c6ebb2c
Duy <pkduy2006@gmail.com>
2024-11-04 10:07:08 +0200

    module 11 and module 12 changes

--- module12/task2.py
+++ module12/task2.py
@@ -1,8 +1,7 @@
 import requests
 
-api_key = "?"
 city_name = input("Enter the city name: ")
-request = "https://api.openweathermap.org/data/2.5/weather?q=" + city_name + "&appid=" + api_key + "&units=metric"
+request = "https://api.openweathermap.org/data/2.5/weather?q=" + city_name + "&appid=33dd23764fa14c0bd3a056f84cae3ea0&units=metric"
 try:
     response = requests.get(request)
     if not response.ok:
